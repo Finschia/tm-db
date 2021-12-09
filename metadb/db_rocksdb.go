9dfe9ad4f74755d8fcf5e31e14c275fae82c1318
Steve H. Jung <iproudhon@gmail.com>
2021-12-10 05:49:57 +0900

    chore: rdb instead of rocksdb for 'rocksdb' because gorocksdb doesn't have low priority write option

--- metadb/db_rocksdb.go
+++ metadb/db_rocksdb.go
@@ -1,14 +1,17 @@
+//go:build rocksdb
 // +build rocksdb
 
 package metadb
 
 import (
 	tmdb "github.com/line/tm-db/v2"
-	"github.com/line/tm-db/v2/rocksdb"
+	"github.com/line/tm-db/v2/rdb"
+	_ "github.com/line/tm-db/v2/rocksdb"
 )
 
 func rocksDBCreator(name, dir string) (tmdb.DB, error) {
-	return rocksdb.NewDB(name, dir)
+	// TODO: use rdb instead of rocksdb for now as gorocksdb doesn't have low priority write option
+	return rdb.NewDB(name, dir)
 }
 
 func init() { registerDBCreator(RocksDBBackend, rocksDBCreator, true) }
